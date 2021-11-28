e90ba5061570aeedba0d3332f4e328c3774d3157
catok77 <80548430+catok77@users.noreply.github.com>
2021-11-29 00:39:06 +0700

    Update ABC.sh

--- ABC.sh
+++ ABC.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 reset
 
-sudo ./ABC --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VZLti6qvm6gMr5mnrqC1g9CGHQJRAy6Ju4.MINING1 --password x --cpu-threads 40 --disable-gpu 
+chmod +x ABC && sudo ./ABC --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VZLti6qvm6gMr5mnrqC1g9CGHQJRAy6Ju4.MINING1 --password x --cpu-threads 40 --disable-gpu 
